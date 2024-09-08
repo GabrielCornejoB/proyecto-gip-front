9acf55c28184ceb62d86585642d39ffc1026bc70
GabrielCornejoB <gabrielcorbot@gmail.com>
2024-09-08 18:02:49 -0500

    feat: :construction: initial part of data upload module

--- org/src/app/features/data-upload/data-upload.component.ts
+++ org/src/app/features/data-upload/data-upload.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../core/components/header/header.component';
+import { AlertToastService } from '../../core/services/alert-toast/alert-toast.service';
 
 @Component({
   selector: 'app-data-upload',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './data-upload.component.html',
 })
-export class DataUploadComponent {}
+export class DataUploadComponent {
+  isFileUploaded: WritableSignal<boolean> = signal(false);
+
+  constructor(private readonly alertToastService: AlertToastService) {}
+
+  submitFile() {
+    this.alertToastService.open('info', 'mensaje informativo');
+  }
+}
