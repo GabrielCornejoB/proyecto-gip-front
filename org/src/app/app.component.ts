0e57c5be453c2c1bc855f513fe1750d50c8a7e2a
GabrielCornejoB <gabrielcorbot@gmail.com>
2024-09-08 18:01:57 -0500

    feat: :sparkles: implement alert toast on main component

--- org/src/app/app.component.ts
+++ org/src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SideBarComponent } from './core/components/side-bar/side-bar.component';
+import { AlertToastService } from './core/services/alert-toast/alert-toast.service';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, SideBarComponent],
+  imports: [RouterModule, SideBarComponent, NgIf, NgClass],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  isAlertToastOpen;
+  alertToastVariant;
+  alertToastText;
+
+  constructor(private readonly alertToastService: AlertToastService) {
+    this.isAlertToastOpen = this.alertToastService.isOpen;
+    this.alertToastVariant = this.alertToastService.variant;
+    this.alertToastText = this.alertToastService.text;
+  }
+
+  closeAlertToast(): void {
+    this.alertToastService.close();
+  }
+}
