4c13ed0068f0367b4259d45e1a613500f081ad14
GabrielCornejoB <gabrielcorbot@gmail.com>
2024-09-08 19:09:16 -0500

    feat: :sparkles: ui to submit file + validations

--- org/src/app/features/data-upload/data-upload.component.ts
+++ org/src/app/features/data-upload/data-upload.component.ts
@@ -1,20 +1,34 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../core/components/header/header.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
 import { AlertToastService } from '../../core/services/alert-toast/alert-toast.service';
 
 @Component({
   selector: 'app-data-upload',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, FileUploadComponent],
   templateUrl: './data-upload.component.html',
 })
 export class DataUploadComponent {
-  isFileUploaded: WritableSignal<boolean> = signal(false);
-
   constructor(private readonly alertToastService: AlertToastService) {}
 
-  submitFile() {
-    this.alertToastService.open('info', 'mensaje informativo');
+  handleSubmitButtonClicked(file: File | undefined): void {
+    if (!file) {
+      return this.alertToastService.open(
+        'warning',
+        'No ha seleccionado ningún archivo',
+      );
+    }
+    if (
+      !file.name.endsWith('.xlsx') ||
+      !file.name.endsWith('.xls') ||
+      !file.name.endsWith('.csv')
+    )
+      return this.alertToastService.open(
+        'warning',
+        'El archivo no es de un formato valido. Debe ser: .xlsx, .xls o .csv',
+      );
+    this.alertToastService.open('success', 'Archivo enviado exitosamente');
   }
 }
