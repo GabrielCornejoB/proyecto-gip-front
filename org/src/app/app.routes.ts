e07d8034cdae75cbba05ce859eeca21d6515ca58
GabrielCornejoB <gabrielcorbot@gmail.com>
2024-09-14 11:36:44 -0500

    refactor: :recycle: refactor dashboard wrapper

--- org/src/app/app.routes.ts
+++ org/src/app/app.routes.ts
@@ -1,11 +1,25 @@
 import { Route } from '@angular/router';
+import { DashboardWrapperComponent } from './core/components/dashboard-wrapper/dashboard-wrapper.component';
 
 export const appRoutes: Route[] = [
   {
-    path: 'data-upload',
+    path: 'login',
     loadComponent: () =>
-      import('./features/data-upload/data-upload.component').then(
-        (c) => c.DataUploadComponent,
+      import('./features/authentication/login/login.component').then(
+        (c) => c.LoginComponent,
       ),
+  },
+  {
+    path: 'dashboard',
+    component: DashboardWrapperComponent,
+    loadChildren: () => [
+      {
+        path: 'data-upload',
+        loadComponent: () =>
+          import('./features/data-upload/data-upload.component').then(
+            (c) => c.DataUploadComponent,
+          ),
+      },
+    ],
   },
 ];
