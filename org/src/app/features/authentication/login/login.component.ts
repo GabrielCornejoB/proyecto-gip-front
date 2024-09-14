adfb29a5634d8ad7a28a621e21e18c5be897d611
GabrielCornejoB <gabrielcorbot@gmail.com>
2024-09-14 14:01:48 -0500

    feat: :sparkles: add login screen

--- org/src/app/features/authentication/login/login.component.ts
+++ org/src/app/features/authentication/login/login.component.ts
@@ -1,11 +1,55 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { SupabaseAuthService } from '../services/supabase-auth/supabase-auth.service';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AlertToastService } from '../../../core/services/alert-toast/alert-toast.service';
+import { LoginForm, loginForm } from './login-form.constant';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form: FormGroup<LoginForm> = loginForm;
+  loading: boolean = false;
+
+  constructor(
+    private readonly supabaseAuthService: SupabaseAuthService,
+    private readonly alertToastService: AlertToastService,
+    private readonly router: Router,
+  ) {}
+
+  async handleFormSubmit() {
+    if (
+      this.form.controls.email.errors ||
+      this.form.controls.password.errors ||
+      !this.form.controls.email.value ||
+      !this.form.controls.password.value
+    ) {
+      this.form.reset();
+      this.form.controls.email.markAsDirty();
+      this.form.controls.password.markAsDirty();
+      return;
+    }
+
+    this.loading = true;
+    const { error } = await this.supabaseAuthService.login(
+      this.form.controls.email.value,
+      this.form.controls.password.value,
+    );
+    this.loading = false;
+    if (error) {
+      return this.alertToastService.open(
+        'error',
+        error.code === 'invalid_credentials'
+          ? 'Credenciales incorrectas.'
+          : 'Ocurrió un error durante la autenticación.',
+      );
+    }
+    await this.router.navigateByUrl('/dashboard/data-upload');
+    return this.alertToastService.open('success', 'Inicio de sesión exitoso');
+  }
+}
---
+++ org/src/app/features/authentication/services/supabase-auth/supabase-auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SupabaseAuthService {
+  supabaseClient: SupabaseClient;
+
+  constructor() {
+    this.supabaseClient = createClient(
+      environment.SUPABASE_URL,
+      environment.SUPABASE_KEY,
+    );
+  }
+
+  async login(email: string, password: string) {
+    return await this.supabaseClient.auth.signInWithPassword({
+      email,
+      password,
+    });
+  }
+}
