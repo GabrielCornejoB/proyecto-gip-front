279ffebfbd7fb28591c80eac9aa827ed148d03cf
GabrielCornejoB <gabrielcorbot@gmail.com>
2024-09-15 20:09:24 -0500

    chore: :closed_lock_with_key: add localhost secret

--- org/src/environments/environment.ts
+++ org/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: '#{API_URL}#',
+  API_URL: 'http://127.0.0.1:8000',
   SUPABASE_URL: process.env['SUPABASE_URL']!,
   SUPABASE_KEY: process.env['SUPABASE_KEY']!,
 };
