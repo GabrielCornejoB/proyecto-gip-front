2100d795b137180dacd4cfb1d8d473ba9946c467
GabrielCornejoB <gabrielcorbot@gmail.com>
2024-10-05 16:20:00 -0500

    feat: :lipstick: add encryption key file input

--- org/src/app/features/data-upload/components/file-upload/file-upload.component.ts
+++ org/src/app/features/data-upload/components/file-upload/file-upload.component.ts
@@ -1,15 +1,20 @@
 import { Component, ElementRef, output, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-file-upload',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './file-upload.component.html',
 })
 export class FileUploadComponent {
   onSubmitButtonClicked = output<FileList | null>();
   @ViewChild('inputFile') fileInput!: ElementRef<HTMLInputElement>;
+  encryptionKeyInput: FormControl = new FormControl('', [Validators.required]);
+
+  readonly inputTooltipText =
+    'Esta clave de encriptación es la que se usará para encriptar las cédulas en los archivos. Recuerda que los archivos originales nunca serán enviados a ninguna base de datos ni servidor. Solamente los archivos después de la encriptación';
 
   submit(): void {
     this.onSubmitButtonClicked.emit(this.fileInput.nativeElement.files);
