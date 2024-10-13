e96be86a0c997a1e6e4d85b8b24fcd310cdc1bc6
GabrielCornejoB <gabrielcorbot@gmail.com>
2024-10-13 09:00:56 -0500

    test: :white_check_mark: create tests for sidebar component & guards

--- org/src/app/core/components/side-bar/side-bar.component.spec.ts
+++ org/src/app/core/components/side-bar/side-bar.component.spec.ts
@@ -1,13 +1,33 @@
 import { SideBarComponent } from './side-bar.component';
+import { SupabaseAuthService } from '../../../features/authentication/services/supabase-auth/supabase-auth.service';
+import { Router } from '@angular/router';
 
 describe('SideBarComponent', () => {
   let component: SideBarComponent;
 
+  const supabaseAuthService = {
+    logout: jest.fn(),
+  } as never as SupabaseAuthService;
+
+  const router = {
+    navigateByUrl: jest.fn(),
+  } as never as Router;
+
   beforeEach(() => {
-    component = new SideBarComponent();
+    component = new SideBarComponent(supabaseAuthService, router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.menuItems).toBeTruthy();
+  });
+
+  describe('logout()', () => {
+    it('should logout from supabase and redirect to the login page', async () => {
+      await component.logout();
+
+      expect(supabaseAuthService.logout).toHaveBeenCalled();
+      expect(router.navigateByUrl).toHaveBeenCalledWith('/auth/login');
+    });
   });
 });
