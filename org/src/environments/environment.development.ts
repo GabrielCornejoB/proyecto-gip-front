55bc0b10e3832cc9a501455820d7140774292144
GabrielCornejoB <gabrielcorbot@gmail.com>
2024-10-13 11:35:24 -0500

    test: :white_check_mark: add tests for auth

--- org/src/environments/environment.development.ts
+++ org/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   API_URL: 'http://127.0.0.1:8000',
+  SUPABASE_KEY: process.env['SUPABASE_URL']!,
+  SUPABASE_URL: process.env['SUPABASE_KEY']!,
 };
